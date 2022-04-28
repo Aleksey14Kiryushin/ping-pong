40a509543fbf6146719284a61855d2c6c3f1a1d4
l_keey14 <Leon062007@mail.ru>
2022-04-28 17:14:58 +0300

    Beginning

--- ping-pong.py
+++ ping-pong.py
@@ -1,2 +1,32 @@
 from pygame import *
-print(100)
+import os
+
+clock = time.Clock()
+
+x = 200
+y = 100
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x,y)
+
+# Window
+global_height = 700
+global_width = 700
+window = display.set_mode((global_width,global_height))
+
+display.set_caption("Shooter")
+display.set_icon(image.load("background.jpg"))
+background = transform.scale(image.load("background.jpg"), (global_height, global_width))
+
+# Variables
+game_Play = True
+
+while game_Play:
+
+    window.blit(background, (0,0))
+
+    for event_get in event.get():
+        if event_get.type == QUIT:
+            game_Play = False
+
+    display.update()
+
+    clock.tick(60)
