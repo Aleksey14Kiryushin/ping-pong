f0f63e6765b2adc00da1b99ede0ed45a995fb049
l_keey14 <Leon062007@mail.ru>
2022-04-28 18:29:05 +0300

    Base-Game

--- ping-pong.py
+++ ping-pong.py
@@ -1,20 +1,153 @@
 from pygame import *
+from random import *
 import os
 
 clock = time.Clock()
 
+# Place
 x = 200
 y = 100
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x,y)
 
+# Object
+class Character(sprite.Sprite):
+    def __init__(self, width, height, x, y, picture, speed):
+        super().__init__()
+        self.image = transform.scale(image.load(picture), (width,height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.picture = picture
+        self.speed = speed
+        self.width = width
+        self.height = height
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+# Player
+class Player(Character):
+    def __init__(self, width, height, x, y, picture, speed, skipped, count, amount_of_lives=3):
+        super().__init__(width, height, x, y, picture[0], speed)
+
+        self.picture = picture
+        self.counter = 0
+
+        self.x_speed = speed
+        self.y_speed = speed 
+
+        self.skipped = skipped
+        self.count = count
+
+        self.amount_of_lives = amount_of_lives
+
+        self.charges = 6
+
+    def update(self):
+
+        key_pressed = key.get_pressed()
+        
+        if key_pressed[K_UP] and self.rect.y > 0: 
+            self.rect.y -= self.y_speed
+
+        if key_pressed[K_DOWN] and self.rect.y < 700:
+            self.rect.y += self.y_speed
+
+class Enemy(Character):
+    def __init__(self, width, height, x, y, picture, speed, skipped, count, amount_of_lives=3):
+        super().__init__(width, height, x, y, picture[0], speed)
+
+        self.picture = picture
+        self.counter = 0
+
+        self.x_speed = speed
+        self.y_speed = speed 
+
+        self.skipped = skipped
+        self.count = count
+
+        self.amount_of_lives = amount_of_lives
+
+        self.charges = 6
+
+    def update(self):
+
+        key_pressed = key.get_pressed()
+        
+        if key_pressed[K_w] and self.rect.y > 0: 
+            self.rect.y -= self.y_speed
+
+        if key_pressed[K_s] and self.rect.y < 700:
+            self.rect.y += self.y_speed
+
+class Puck(Character):
+    def __init__(self, width, height, x, y, picture, speed, health=1, returning=False):
+        super().__init__(width, height, x, y, picture[0], speed)
+    
+        self.speed_x = speed 
+
+        self.speed_y = speed
+
+        self.returning_back = True
+
+        self.returning_top = True
+    def update(self):
+        # Касание левой и правой стены:
+        self.rect.x += self.speed_x 
+        
+        # if self.rect.x >= 750:
+        #     self.returning_back = False
+
+        # if self.rect.x <= 25:
+        #     self.returning_back = True
+
+        # if self.returning_back:
+        #     self.rect.x += self.speed_x 
+            
+        # if not self.returning_back:        
+        #     self.rect.x -= self.speed_x 
+        
+        # Касание верхней и нижней стены:
+        if self.rect.y >= 775:
+            self.returning_top = False
+
+        if self.rect.y <= 25:
+            self.returning_top = True
+
+        if self.returning_top:
+            self.rect.y += self.speed_y 
+
+        if not self.returning_top:
+            self.rect.y -= self.speed_y 
+
+        # Столкновение с игроком
+        if sprite.collide_rect(player_1st, self) or sprite.collide_rect(player_2nd, self):
+            self.speed_x *= -1
+            
+    def random_position(self):
+        self.rect.x = randint(50, 650)
+        self.rect.y = 50
+
 # Window
-global_height = 700
-global_width = 700
+global_height = 800
+global_width = 800
 window = display.set_mode((global_width,global_height))
 
-display.set_caption("Shooter")
+display.set_caption("Ping-Pong")
 display.set_icon(image.load("background.jpg"))
 background = transform.scale(image.load("background.jpg"), (global_height, global_width))
+
+# Players
+pictures_players = ["reflector.png"]
+
+player_1st = Player(100, 100, 0, 400, pictures_players, 10, 0, 0)
+
+player_2nd = Enemy(100, 100, 700, 400, pictures_players, 10, 0, 0)
+
+# Puck
+picture_puck = ["puck.png"]
+
+puck = Puck(50, 50, 400, 500, picture_puck, 3)
 
 # Variables
 game_Play = True
@@ -26,6 +159,23 @@ while game_Play:
     for event_get in event.get():
         if event_get.type == QUIT:
             game_Play = False
+
+    if puck.rect.x <= 10:
+        print("1ST LOST!")
+
+    if puck.rect.x >= 785:
+        print("2RD LOST!")
+
+# Players
+    player_1st.reset()
+    player_1st.update()
+
+    player_2nd.reset()
+    player_2nd.update()
+
+# Puck
+    puck.reset()
+    puck.update()
 
     display.update()
 
